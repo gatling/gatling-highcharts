fc255df56327ced589eb0c3fcdf114ed41bceff8
slandelle <slandelle+github@excilys.com>
2011-12-13 12:27:10 +0100

    Fix archetype

--- gatling-highcharts-maven-archetype/src/main/resources/archetype-resources/src/main/scala/IDEPathHelper.scala
+++ gatling-highcharts-maven-archetype/src/main/resources/archetype-resources/src/main/scala/IDEPathHelper.scala
@@ -11,5 +11,5 @@ object IDEPathHelper {
 	val requestBodiesFolder = projectDir / "src/main/resources/request-bodies"
 	val eclipseAssetsFolder = projectDir / "src/main/resources/assets"
 	val eclipseSimulationFolder = projectDir / "src/main/scala" / packageName.replace(".", "/")
-	val outputFolder = projectDir / "src/main/scala" / "com.excilys.ebi.gatling.toto".replace(".", "/")
+	val outputFolder = projectDir / "src/main/scala" / "${package}".replace(".", "/")
 }
