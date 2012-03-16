0cf2cc6b20bafd6ffdffb4f55f87f4f213320b3c
Stephane Landelle <slandelle+github@excilys.com>
2012-03-16 10:50:31 +0100

    Don't jump to RunningFrame, close excilys/gatling/#437

--- gatling-highcharts-maven-archetype/src/main/resources/archetype-resources/src/main/scala/Recorder.scala
+++ gatling-highcharts-maven-archetype/src/main/resources/archetype-resources/src/main/scala/Recorder.scala
@@ -1,9 +1,13 @@
+import scala.tools.nsc.io.Path.string2path
+import scala.tools.nsc.io.File
+
 import com.excilys.ebi.gatling.core.util.PathHelper.path2string
+import com.excilys.ebi.gatling.recorder.configuration.CommandLineOptionsConstants.{ REQUEST_BODIES_FOLDER_OPTION, PACKAGE_OPTION, OUTPUT_FOLDER_OPTION }
 import com.excilys.ebi.gatling.recorder.ui.GatlingHttpProxyUI
 
 import IDEPathHelper.{ requestBodiesFolder, recorderOutputFolder }
 
 object Recorder extends App {
 
-	GatlingHttpProxyUI.main(Array("-of", recorderOutputFolder, "-run", "-ide", "${package}", "-bf", requestBodiesFolder))
+	GatlingHttpProxyUI.main(Array(OUTPUT_FOLDER_OPTION, recorderOutputFolder/ "${package}".replace(".", File.separator), PACKAGE_OPTION, "${package}", REQUEST_BODIES_FOLDER_OPTION, requestBodiesFolder))
 }
