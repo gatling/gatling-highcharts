0bceb60f3b487486a41b14684ef5e1aef2e3a31b
Stephane Landelle <slandelle+github@excilys.com>
2012-03-22 19:16:02 +0100

    Fix recorder output folder

--- gatling-highcharts-maven-archetype/src/main/resources/archetype-resources/src/main/scala/Recorder.scala
+++ gatling-highcharts-maven-archetype/src/main/resources/archetype-resources/src/main/scala/Recorder.scala
@@ -1,6 +1,3 @@
-import scala.tools.nsc.io.Path.string2path
-import scala.tools.nsc.io.File
-
 import com.excilys.ebi.gatling.core.util.PathHelper.path2string
 import com.excilys.ebi.gatling.recorder.configuration.CommandLineOptionsConstants.{ REQUEST_BODIES_FOLDER_OPTION, PACKAGE_OPTION, OUTPUT_FOLDER_OPTION }
 import com.excilys.ebi.gatling.recorder.ui.GatlingHttpProxyUI
@@ -9,5 +6,5 @@ import IDEPathHelper.{ requestBodiesFolder, recorderOutputFolder }
 
 object Recorder extends App {
 
-	GatlingHttpProxyUI.main(Array(OUTPUT_FOLDER_OPTION, recorderOutputFolder/ "${package}".replace(".", File.separator), PACKAGE_OPTION, "${package}", REQUEST_BODIES_FOLDER_OPTION, requestBodiesFolder))
+	GatlingHttpProxyUI.main(Array(OUTPUT_FOLDER_OPTION, recorderOutputFolder, PACKAGE_OPTION, "${package}", REQUEST_BODIES_FOLDER_OPTION, requestBodiesFolder))
 }
