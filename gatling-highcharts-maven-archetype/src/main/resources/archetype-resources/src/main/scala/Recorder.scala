c1642d498a811676579a818d346a5a5aa7798e20
BluePyth <bluepyth@gmail.com>
2012-05-12 13:45:12 +0200

    Takes the new Recorder in account

--- gatling-highcharts-maven-archetype/src/main/resources/archetype-resources/src/main/scala/Recorder.scala
+++ gatling-highcharts-maven-archetype/src/main/resources/archetype-resources/src/main/scala/Recorder.scala
@@ -1,10 +1,9 @@
 import com.excilys.ebi.gatling.core.util.PathHelper.path2string
-import com.excilys.ebi.gatling.recorder.configuration.CommandLineOptionsConstants.{ REQUEST_BODIES_FOLDER_OPTION, PACKAGE_OPTION, OUTPUT_FOLDER_OPTION }
-import com.excilys.ebi.gatling.recorder.ui.GatlingHttpProxyUI
+import com.excilys.ebi.gatling.recorder.GatlingRecorder
+import com.excilys.ebi.gatling.recorder.GatlingRecorder._
 
 import IDEPathHelper.{ requestBodiesFolder, recorderOutputFolder }
 
 object Recorder extends App {
-
-	GatlingHttpProxyUI.main(Array(OUTPUT_FOLDER_OPTION, recorderOutputFolder, PACKAGE_OPTION, "${package}", REQUEST_BODIES_FOLDER_OPTION, requestBodiesFolder))
+	GatlingRecorder.main(Array(OUTPUT_FOLDER_OPTION, recorderOutputFolder, PACKAGE_OPTION, "${package}", REQUEST_BODIES_FOLDER_OPTION, requestBodiesFolder))
 }
