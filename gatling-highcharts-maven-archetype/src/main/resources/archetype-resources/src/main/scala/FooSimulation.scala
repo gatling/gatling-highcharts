cadc5d94616c0137e4edb332ae42844e9fd8023c
Stephane Landelle <slandelle+github@excilys.com>
2012-10-01 12:01:05 +0200

    Update archetype with scala-maven-plugin 3.1.0

--- gatling-highcharts-maven-archetype/src/main/resources/archetype-resources/src/main/scala/FooSimulation.scala
+++ gatling-highcharts-maven-archetype/src/main/resources/archetype-resources/src/main/scala/FooSimulation.scala
@@ -3,6 +3,9 @@ package ${package}
 import com.excilys.ebi.gatling.core.Predef._
 import com.excilys.ebi.gatling.http.Predef._
 import com.excilys.ebi.gatling.jdbc.Predef._
+import com.excilys.ebi.gatling.http.Headers.Names._
+import akka.util.duration._
+import bootstrap._
 
 class FooSimulation extends Simulation {
 
