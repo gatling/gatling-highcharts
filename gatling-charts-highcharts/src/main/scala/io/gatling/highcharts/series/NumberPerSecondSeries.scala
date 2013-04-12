c8f8a4cee96038b7aa982f42b9b81ae4294d210e
Stephane Landelle <slandelle+github@excilys.com>
2013-04-12 20:47:25 +0200

    Minor clean up: pimp

--- gatling-charts-highcharts/src/main/scala/io/gatling/highcharts/series/NumberPerSecondSeries.scala
+++ gatling-charts-highcharts/src/main/scala/io/gatling/highcharts/series/NumberPerSecondSeries.scala
@@ -6,9 +6,9 @@
 package io.gatling.highcharts.series
 
 import io.gatling.core.result.{ IntVsTimePlot, Series }
-import io.gatling.core.util.StringHelper.escapeJsQuoteString
+import io.gatling.core.util.StringHelper.RichString
 
-class NumberPerSecondSeries(name: String, runStart: Long, data: Seq[IntVsTimePlot], color: String) extends Series[IntVsTimePlot](escapeJsQuoteString(name), data, List(color)) {
+class NumberPerSecondSeries(name: String, runStart: Long, data: Seq[IntVsTimePlot], color: String) extends Series[IntVsTimePlot](name.escapeJsQuoteString, data, List(color)) {
 
 	def elements: Seq[String] = data.map { plot => "[" + (runStart + plot.time) + "," + plot.value + "]" }
 }
