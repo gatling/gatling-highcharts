eb6e16899d72bc14b936acca345ec46bb2f4e431
Stephane Landelle <slandelle+github@excilys.com>
2013-08-01 15:35:55 +0200

    Minor clean up

--- gatling-charts-highcharts/src/main/scala/io/gatling/highcharts/component/GroupDetailsDurationDistributionComponent.scala
+++ gatling-charts-highcharts/src/main/scala/io/gatling/highcharts/component/GroupDetailsDurationDistributionComponent.scala
@@ -13,8 +13,8 @@ object GroupDetailsDurationDistributionComponent {
 
 	def apply(title: String, containerId: String, durationDistributionSuccess: Series[IntVsTimePlot], durationDistributionFailure: Series[IntVsTimePlot]) = {
 		val template = new GroupDetailsDurationDistributionTemplate(
-		    title,
-		    containerId,
+			title,
+			containerId,
 			new StackedColumnSeries(durationDistributionSuccess.name, durationDistributionSuccess.data.map { plot => PieSlice(plot.time.toString, plot.value) }, durationDistributionSuccess.colors.head),
 			new StackedColumnSeries(durationDistributionFailure.name, durationDistributionFailure.data.map { plot => PieSlice(plot.time.toString, plot.value) }, durationDistributionFailure.colors.head))
 
