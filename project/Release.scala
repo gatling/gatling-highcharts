0f1afc7e86d0e37573ddfb85de6e5bb72a6ed0c6
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2014-09-29 16:36:47 +0200

    Do not check for snapshot dependencies

--- project/Release.scala
+++ project/Release.scala
@@ -1,16 +1,37 @@
 import scala.util.Properties.propOrEmpty
 
+import sbt._
+import sbt.Keys._
 import com.typesafe.sbt.SbtPgp.PgpKeys._
+import sbtrelease._
 import sbtrelease.ReleasePlugin._
 import sbtrelease.ReleasePlugin.ReleaseKeys._
+import sbtrelease.ReleaseStateTransformations._
 
 object Release {
 
   lazy val settings = releaseSettings ++ Seq(
     crossBuild := false,
-    publishArtifactsAction := publishSigned.value,
     releaseVersion := { _ => propOrEmpty("releaseVersion")},
-    nextVersion := { _ => propOrEmpty("developmentVersion")}
+    nextVersion := { _ => propOrEmpty("developmentVersion")},
+    releaseProcess := Seq[ReleaseStep](
+      inquireVersions,
+      runClean,
+      runTest,
+      setReleaseVersion,
+      commitReleaseVersion,
+      tagRelease,
+      publishSignedStep,
+      setNextVersion,
+      commitNextVersion,
+      pushChanges
+    )
   )
 
+  private val publishSignedStep = ReleaseStep(
+    action = st => {
+      val extracted = Project.extract(st)
+      val ref = extracted.get(thisProjectRef)
+      extracted.runAggregated(publishSigned in Global in ref, st)
+    })
 }
