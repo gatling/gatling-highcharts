826b3eca39ce94187f600315601064adfe0eac31
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2014-10-24 11:57:01 +0200

    Build: Enable consolidated resolution

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -16,7 +16,7 @@ object BuildSettings {
     startYear            := Some(2011),
     licenses             := Seq("Gatling Highcharts License" -> new URL("https://raw.github.com/gatling/gatling-highcharts/master/src/main/resources/META-INF/LICENSE")),
     resolvers            := envOrNone("CI").map(_ => Seq(Opts.resolver.sonatypeSnapshots)).getOrElse(Seq(Resolver.mavenLocal)),
-    updateOptions        := updateOptions.value.withLatestSnapshots(false),
+    updateOptions        := updateOptions.value.withLatestSnapshots(false).withConsolidatedResolution(true),
     scalaVersion         := "2.11.2",
     scalacOptions        := Seq(
       "-encoding",
