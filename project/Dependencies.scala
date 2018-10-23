ed9823b619e88ca93ce18ffa4c1a5c28c3d213ed
Stephane Landelle <slandelle@gatling.io>
2018-10-23 12:52:34 +0200

    Force gatling-commons and gatling-http-client deps
    There's most likely a bug in sbt or ivy, and sometimes those deps are missing from fullClasspath in Runtime.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,15 +5,17 @@ object Dependencies {
   private val bundleArtifact = Artifact("gatling-bundle", "zip", "zip", "bundle")
 
   // format: OFF
-  def charts(gatlingVersion: String)      = "io.gatling"     % "gatling-charts"   % gatlingVersion
-  def app(gatlingVersion: String)         = "io.gatling"     % "gatling-app"      % gatlingVersion
-  def compiler(gatlingVersion: String)    = "io.gatling"     % "gatling-compiler" % gatlingVersion
-  def recorder(gatlingVersion: String)    = "io.gatling"     % "gatling-recorder" % gatlingVersion
-  def bundle(gatlingVersion: String)      = "io.gatling"     % "gatling-bundle"   % gatlingVersion % "runtime" artifacts bundleArtifact
+  def charts(gatlingVersion: String)      = "io.gatling"     % "gatling-charts"      % gatlingVersion
+  def commons(gatlingVersion: String)     = "io.gatling"     % "gatling-commons"     % gatlingVersion
+  def httpClient(gatlingVersion: String)  = "io.gatling"     % "gatling-http-client" % gatlingVersion
+  def app(gatlingVersion: String)         = "io.gatling"     % "gatling-app"         % gatlingVersion
+  def compiler(gatlingVersion: String)    = "io.gatling"     % "gatling-compiler"    % gatlingVersion
+  def recorder(gatlingVersion: String)    = "io.gatling"     % "gatling-recorder"    % gatlingVersion
+  def bundle(gatlingVersion: String)      = "io.gatling"     % "gatling-bundle"      % gatlingVersion artifacts bundleArtifact
   // format: ON
 
   def gatlingChartsHighchartsDeps(version: String) =
-    Seq(charts _, app _, recorder _).map(_(version))
+    Seq(charts _, app _, recorder _, httpClient _, commons _).map(_(version))
 
   def gatlingHighchartsBundleDeps(gatlingVersion: String, scalaVersion: String) =
     Seq(bundle _, compiler _).map(_(gatlingVersion))
