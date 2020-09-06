5dedde7babbc2c1cd69ba821f50c3c0afe4241f6
Stephane Landelle <slandelle@gatling.io>
2020-09-06 20:54:55 +0200

    nit: clean up deps that are pulled transitively anyway

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,6 @@ object Dependencies {
   private val bundleArtifact = Artifact("gatling-bundle", "zip", "zip", "bundle")
 
   // format: OFF
-  def charts(gatlingVersion: String)      = "io.gatling"     % "gatling-charts"      % gatlingVersion
-  def commons(gatlingVersion: String)     = "io.gatling"     % "gatling-commons"     % gatlingVersion
-  def httpClient(gatlingVersion: String)  = "io.gatling"     % "gatling-http-client" % gatlingVersion
   def app(gatlingVersion: String)         = "io.gatling"     % "gatling-app"         % gatlingVersion
   def compiler(gatlingVersion: String)    = "io.gatling"     % "gatling-compiler"    % gatlingVersion
   def recorder(gatlingVersion: String)    = "io.gatling"     % "gatling-recorder"    % gatlingVersion
@@ -15,7 +12,7 @@ object Dependencies {
   // format: ON
 
   def gatlingChartsHighchartsDeps(version: String) =
-    Seq(charts _, app _, recorder _, httpClient _, commons _).map(_(version))
+    Seq(app _, recorder _).map(_(version))
 
   def gatlingHighchartsBundleDeps(gatlingVersion: String, scalaVersion: String) =
     Seq(bundle _, compiler _).map(_(gatlingVersion))
