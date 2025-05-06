65f769f7f397f6801623cc9a41d0fde71ee5ddbe
slandelle <slandelle@gatling.io>
2025-05-07 00:41:38 +0200

    fix: fix polar chart

--- src/main/scala/io/gatling/charts/highcharts/template/RequestCountPolarTemplate.scala
+++ src/main/scala/io/gatling/charts/highcharts/template/RequestCountPolarTemplate.scala
@@ -19,8 +19,8 @@ private[highcharts] final class RequestCountPolarTemplate(rootContainer: GroupCo
     val requestContainers = collectRequestContainersRec(rootContainer)
     val ticksInterval = math.ceil(requestContainers.size / 1000.0)
     val categories = requestContainers.map(_.name).mkString("['", "', '", "']")
-    val okData = requestContainers.map(_.stats.numberOfRequestsStatistics.success).mkString("[", ", ", "]")
-    val koData = requestContainers.map(_.stats.numberOfRequestsStatistics.failure).mkString("[", ", ", "]")
+    val okData = requestContainers.map(container => container.stats.numberOfRequestsStatistics.success.getOrElse(0L)).mkString("[", ", ", "]")
+    val koData = requestContainers.map(container => container.stats.numberOfRequestsStatistics.failure.getOrElse(0L)).mkString("[", ", ", "]")
 
     s"""
 var tickInterval = $ticksInterval;
